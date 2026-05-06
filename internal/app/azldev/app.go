5f6a66cf2123fe6f4352b34652d5ba9a163f1b4b
Daniel McIlvaney <damcilva@microsoft.com>
2026-05-06 16:58:44 -0700

    feat: Add root user check escape hatch with env var (#163)

--- internal/app/azldev/app.go
+++ internal/app/azldev/app.go
@@ -119,7 +119,8 @@ lives), or use -C to point to one.`,
 		PersistentPreRunE: func(command *cobra.Command, _ []string) error {
 			slog.Debug("Command annotations", "annotations", command.Annotations)
 
-			if _, ok := command.Annotations[CommandAnnotationRootOK]; !ok && os.Geteuid() == 0 {
+			if _, ok := command.Annotations[CommandAnnotationRootOK]; !ok && os.Geteuid() == 0 &&
+				app.osEnvFactory.OSEnv().Getenv("AZLDEV_ALLOW_ROOT") != "1" {
 				return errors.New("this command may not be run as root")
 			}
 
